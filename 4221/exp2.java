2d08fa407a22b4a3ffd4c35318c7fcfaa13f4528
ShaikShaheera07 <228a1a4221@risekrishnasaiprakasam.edu.in>
2024-10-23 11:04:23 +0530

    Add files via upload

---
+++ 4221/exp2.java
@@ -0,0 +1,8 @@
+public class Apatheu
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Banggin-Banggin wanna crack these walls");
+		System.out.println("Banngin-Banggin wanna Echo through the halls ");
+	}
+}
